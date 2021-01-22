db1706d462de9eeaca3ed94d8317ad548ec6d68f
mseo.39 <a01063294905@gmail.com>
2021-01-22 17:02:29 +0900

    ModelForm

--- 8th_django_review/jss/jss_project/main/forms.py
+++ 8th_django_review/jss/jss_project/main/forms.py
@@ -7,3 +7,15 @@ class JssForm(forms.ModelForm):
     class Meta:
         model = Jasoseol
         fields = ('title', 'content',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) #부모 클래스에 있는 것을 가져와서 쓴다
+        self.fields['title'].label ="제목"
+        self.fields['content'].label ="자기소개서 내용"
+        self.fields['title'].widget.attrs.update({
+            'class':'jss_title',
+            'placeholder':'제목',
+        })
+        self.fields['content'].widget.attrs.update({
+            'class': 'jss_content_form',
+        })
