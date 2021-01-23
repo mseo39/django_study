e4d6b208393c772564b49ea773c7734bd4bcc5f4
mseo.39 <a01063294905@gmail.com>
2021-01-23 18:35:33 +0900

    pk_detail

--- 8th_django_review/jss/jss_project/jss_project/urls.py
+++ 8th_django_review/jss/jss_project/jss_project/urls.py
@@ -20,5 +20,8 @@ import main.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main.views.index, name="index"),
+    path('detail/<int:jss_id>',main.views.detail,name="detail"),
     path('create', main.views.create, name="create"),
+    path('delete/<int:jss_id>',main.views.delete, name="delete"),
+    path('update/<int:jss_id>',main.views.update, name="update"),
 ]
--- 8th_django_review/jss/jss_project/main/views.py
+++ 8th_django_review/jss/jss_project/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import JssForm
 from .models import Jasoseol
 # Create your views here.
@@ -17,3 +17,30 @@ def create(request):
 
     jss_form=JssForm()
     return render(request, 'create.html', {'jss_form':jss_form})
+
+def detail(request, jss_id):
+
+    # try: #페이지가 있다면
+    #     my_jss = Jasoseol.object.get(pk=jss_id) #.get으로 하나의 object만 보내줌
+
+    # except:#페이지가 없다면
+    #     raise Http404
+
+    my_jss=get_object_or_404(Jasoseol, pk=jss_id)
+    return render(request, 'detail.html', {'my_jss':my_jss})
+
+def delete(request, jss_id):
+    my_jss = Jasoseol.objects.get(pk=jss_id)
+    my_jss.delete()
+    return redirect('index')
+        
+
+def update(request, jss_id):
+    my_jss = Jasoseol.objects.get(pk=jss_id)
+    jss_form = JssForm(instance=my_jss)
+    if request.method == "POST":
+        updated_form= JssForm(request.POST, instance=my_jss) #instance를 작성해줘야 수정하고 싶은 오브젝트에 수정할 수 있음
+        if updated_form.is_valid():
+            updated_form.save()
+            return redirect('index')
+    return render(request,'create.html',{'jss_form':jss_form})
