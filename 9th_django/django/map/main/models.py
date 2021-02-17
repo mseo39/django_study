3d27fb326f52b1ebc26101f17e8491777aed7a73
mseo.39 <a01063294905@gmail.com>
2021-02-17 18:49:42 +0900

    thumbnail

--- 9th_django/django/map/main/models.py
+++ 9th_django/django/map/main/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from imagekit.models import ImageSpaceField
+from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFill
 # Create your models here.
 
 class Pictures(models.Model):
     text = models.TextField()
     image= models.ImageField(upload_to="blogimg")
-    image_thumbnail = ImageSpaceField(source='image', processors=[ResizeToFill(120,60)])
+    image_thumbnail = ImageSpecField(source='image', processors=[ResizeToFill(120,60)])
     #가로 세로
     #format='JPEG' option={'quality':60}
--- 9th_django/django/map/map/settings.py
+++ 9th_django/django/map/map/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
     #privider
     'allauth.socialaccount.providers.google',
-    'imagekit'
+    'imagekit',
 ]
 
 MIDDLEWARE = [
